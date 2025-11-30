25a59a2174655cb9ab60be4ffe97b473a20305e8
0sad4y <ilyaosadchiy02@gmail.com>
2025-11-30 19:05:42 +0300

    Update web templates and scripts

---
+++ web/static/js/charts.js
@@ -0,0 +1,62 @@
+function LongitudeChart() {
+    const labels = chartData1.map(d => d.x);
+    const data = chartData1.map(d => d.y);
+
+    const ctx = document.getElementById('lineChart').getContext('2d');
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: '',
+                data: data,
+                borderColor: '#d63384',
+                backgroundColor: 'rgba(214, 51, 132, 0.2)',
+                tension: 0.3,
+                pointStyle: false,
+            }]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: { display: false }
+            },
+            scales: {
+                y: { beginAtZero: true }
+            }
+        }
+    });
+}
+
+function DistrictChart() {
+    const labels = chartData1.map(d => d.x);
+    const data = chartData1.map(d => d.y);
+
+    const ctx = document.getElementById('barChart').getContext('2d');
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: '',
+                data: data,
+                backgroundColor: '#ffb6c1',
+                borderRadius: 8
+            }]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: { display: false }
+            },
+            scales: {
+                y: { beginAtZero: true }
+            }
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    LongitudeChart()
+    DistrictChart()
+});
--- web/static/js/tables.js
+++ web/static/js/tables.js
@@ -1,23 +1,10 @@
-const data = [
-    { id: 1, date: "01.01.2025", type: "Тип A", duration: "2 дня", destination: "Куда A", money: 500 },
-    { id: 2, date: "05.01.2025", type: "Тип B", duration: "3 дня", destination: "Куда B", money: 800 },
-    { id: 3, date: "10.01.2025", type: "Тип C", duration: "1 день", destination: "Куда C",  money: 300 },
-    { id: 4, date: "12.01.2025", type: "Тип D", duration: "4 дня", destination: "Куда D", money: 700 },
-    { id: 5, date: "15.01.2025", type: "Тип E", duration: "5 дней", destination: "Куда E",  money: 1200 },
-    { id: 6, date: "18.01.2025", type: "Тип F", duration: "2 дня", destination: "Куда F", money: 400 },
-    { id: 7, date: "20.01.2025", type: "Тип G", duration: "3 дня", destination: "Куда G", money: 900 },
-    { id: 8, date: "22.01.2025", type: "Тип H", duration: "2 дня", destination: "Куда H", money: 600 },
-    { id: 9, date: "25.01.2025", type: "Тип I", duration: "1 день", destination: "Куда I",  money: 250 },
-    { id: 10, date: "28.01.2025", type: "Тип J", duration: "4 дня", destination: "Куда J", money: 1000 }
-];
-
 const rowsPerPage = 5;
 let currentPage = 1;
 
 function renderTable(page = 1) {
     const start = (page - 1) * rowsPerPage;
     const end = start + rowsPerPage;
-    const pageData = data.slice(start, end);
+    const pageData = tableData.slice(start, end);
 
     const tbody = document.getElementById("data-body");
     tbody.innerHTML = "";
@@ -26,17 +13,10 @@ function renderTable(page = 1) {
         const row = `
             <tr>
                 <td>${item.type}</td>
-                <td>${item.date}</td>
-                <td>${item.duration}</td>
-                <td>${item.destination}</td>
-                <td>${item.money}</td>
-                <td>
-                    <a href="/employee/${item.id}">
-                        <button class="btn btn-success btn-sm")">
-                            Открыть
-                        </button>
-                    </a>
-                </td>
+                <td>${item.avgPopulation}</td>
+                <td>${item.childrenPercent}</td>
+                <td>${item.minPopulation}</td>
+                <td>${item.maxPopulation}</td>
             </tr>`;
         tbody.insertAdjacentHTML("beforeend", row);
     });
