75738c0a3497dd532ec13f8cf0789997a554376b
0sad4y <ilyaosadchiy02@gmail.com>
2025-11-30 21:32:24 +0300

    Add data transsfer objects

---
+++ internal/dto/city_dto.go
@@ -0,0 +1,12 @@
+package dto
+
+type CityDTO struct {
+	ID         uint
+	Name       string
+	Type       string
+	District   string
+	Population int
+	Childrens  float32
+	Latitude   float32
+	Longitude  float32
+}
