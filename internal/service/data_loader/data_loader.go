7383918fa768401e2fd7be5a9adeb8f76866e7f8
0sad4y <ilyaosadchiy02@gmail.com>
2025-12-02 19:34:17 +0300

    Add type map and join large cities

--- internal/service/data_loader/data_loader.go
+++ internal/service/data_loader/data_loader.go
@@ -15,6 +15,47 @@ type DataLoader struct {
 	db *gorm.DB
 }
 
+var settlementsTypes = map[string]string{
+	"г":             "город",
+	"гп":            "городской поселок",
+	"д":             "деревня",
+	"дп":            "дачный поселок",
+	"ж/д блокпост":  "железнодорожный блокпост",
+	"ж/д будка":     "железнодорожная будка",
+	"ж/д ветка":     "железнодорожная ветка",
+	"ж/д казарма":   "железнодорожная казарма",
+	"ж/д комбинат":  "железнодорожный комбинат",
+	"ж/д оп":        "железнодорожный остановочный пункт",
+	"ж/д платформа": "железнодорожная платформа",
+	"ж/д площадка":  "железнодорожная площадка",
+	"ж/д пост":      "железнодорожный путевой пост",
+	"ж/д рзд":       "железнодорожный разъезд",
+	"ж/д ст":        "железнодорожная станция",
+	"зим":           "зимовье",
+	"к":             "кишлак",
+	"кп":            "курортный поселок",
+	"л/п":           "лесной поселок",
+	"м":             "местечко",
+	"мкр":           "микрорайон",
+	"нп":            "населенный пункт",
+	"п ж/д ст":      "поселок при железнодорожной станции",
+	"п":             "поселок",
+	"п/ст":          "поселок при станции",
+	"пгт":           "поселок городского типа",
+	"р-н":           "район",
+	"рзд":           "разъезд",
+	"рп":            "рабочий поселок",
+	"с":             "село",
+	"с/п":           "сельское поселение",
+	"сл":            "слобода",
+	"снт":           "садоводческое некоммерческое товарищество",
+	"ст-ца":         "станица",
+	"ст":            "станция",
+	"тер":           "территория",
+	"у":             "улус",
+	"х":             "хутор",
+}
+
 func New(db *gorm.DB) *DataLoader {
 	return &DataLoader{db: db}
 }
@@ -54,10 +95,10 @@ func (dl *DataLoader) LoadCityData(filePath string) error {
 }
 
 func (dl *DataLoader) processRow(record []string) error {
-	// CSV columns: Region, Settlement, Type, Population, Children, Latitude, Longitude,
+	// CSV columns: Region, Settlement, Type, Population, Children, Latitude, Longitude
 	region := strings.TrimSpace(record[1])
 	settlement := strings.TrimSpace(record[3])
-	typ := strings.TrimSpace(record[4])
+	typeShort := strings.TrimSpace(record[4])
 	populationStr := strings.TrimSpace(record[5])
 	childrenStr := strings.TrimSpace(record[6])
 	latitudeStr := strings.TrimSpace(record[9])
@@ -72,6 +113,18 @@ func (dl *DataLoader) processRow(record []string) error {
 		return nil
 	}
 
+	if longitude < 0 {
+		longitude = 180 - longitude
+	}
+
+	typ := ""
+	v, ok := settlementsTypes[typeShort]
+	if ok {
+		typ = v
+	} else {
+		typ = typeShort
+	}
+
 	var typeM models.Type
 	err := dl.db.Where("name=?", typ).FirstOrCreate(&typeM, models.Type{Name: typ}).Error
 	if err != nil {
@@ -82,6 +135,33 @@ func (dl *DataLoader) processRow(record []string) error {
 	err = dl.db.Where("name=?", region).FirstOrCreate(&district, models.District{Name: region}).Error
 	if err != nil {
 		return fmt.Errorf("failed to create/find district: %w", err)
+	}
+
+	if region == settlement {
+		var city models.City
+		err = dl.db.Where("name=? AND latitude=? AND longitude=?", settlement, latitude, longitude).FirstOrCreate(
+			&city,
+			models.City{
+				Name:       settlement,
+				TypeID:     typeM.ID,
+				DistrictID: district.ID,
+				Population: 0,
+				Childrens:  0,
+				Latitude:   latitude,
+				Longitude:  longitude,
+			}).Error
+		if err != nil {
+			return fmt.Errorf("failed to create/find city: %w", err)
+		}
+		city.Population += population
+		city.Childrens += childrens
+
+		err = dl.db.Save(&city).Error
+		if err != nil {
+			return fmt.Errorf("failed to save updated city: %w", err)
+		}
+
+		return nil
 	}
 
 	city := models.City{
